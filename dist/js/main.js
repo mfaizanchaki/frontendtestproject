b2779b1af479813118f4e6a6a80946ecf81fa16f
mfaizanchaki <faizandada170@gmail.com>
2017-09-27 02:45:13 +0500

    Added complete files for frontendtest project

---
+++ dist/js/main.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function(e) {
+    jQuery('.bxslider').bxSlider({
+    mode: 'fade',
+    captions: true
+    });
+
+
+jQuery('.menu_button').click(function(){
+    jQuery('#navbarResponsive').toggle();
+});
+});     
